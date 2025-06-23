4e5cbb398804e7913785d6d6bc79562c05403061
Bertrand <bertrand.tang.dev@gmail.com>
2025-06-23 16:38:48 +0200

    implements handleModifyProduct and adds button on cards

--- src/App.jsx
+++ src/App.jsx
@@ -37,6 +37,20 @@ function App() {
     alert(` Le produit avec l'id ${data.id} a été créé `)
   }
 
+  async function handleModifyProduct() {
+    const response = await fetch("https://fakestoreapi.com/products/1", {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json", // Indique que les données envoyées sont au format JSON
+      },
+      body: JSON.stringify({ // Convertit l'objet JavaScript en une chaîne JSON
+        price: 559.99,
+      }),
+    });
+    const data = await response.json();
+    alert(` Le produit avec l'id ${data.id} a été modifié `)
+  }
+
   return (products &&
 
     <Container>
@@ -59,6 +73,9 @@ function App() {
                   <strong>Prix :</strong> {product.price} €
                 </Card.Text>
               </Card.Body>
+              <Card.Footer className="d-flex justify-content-center">
+                <Button variant="primary" className="d-flex" onClick={handleModifyProduct}>Modifier</Button>
+              </Card.Footer>
             </Card>
           </Col>
         ))}
