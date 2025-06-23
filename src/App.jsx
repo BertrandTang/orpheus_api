d9a5405649a97efa039907cbb752106b9acf6a04
Bertrand <bertrand.tang.dev@gmail.com>
2025-06-23 15:37:33 +0200

    adds handleAddProduct function and button triggering the event, alerts with ID of product created

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Card from 'react-bootstrap/Card';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
 import '../src/assets/styles/app.scss';
 
 
@@ -10,13 +11,31 @@ function App() {
   const [products, setProducts] = useState(null);
 
   useEffect(() => {
-    async function fetchProduct() {
+    async function fetchProducts() {
       const response = await fetch("https://fakestoreapi.com/products");
       const data = await response.json();
       setProducts(data);
     }
-    fetchProduct();
+    fetchProducts();
   }, []);
+
+  async function handleAddProduct() {
+    const response = await fetch("https://fakestoreapi.com/products", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json", // Indique que les données envoyées sont au format JSON
+      },
+      body: JSON.stringify({ // Convertit l'objet JavaScript en une chaîne JSON
+        title: "Naïnetendo Souitch 2",
+        price: 449.99,
+        description: "Un super produit ajouté via API",
+        image: "https://via.placeholder.com/150",
+        category: "electronics",
+      }),
+    });
+    const data = await response.json();
+    alert(` Le produit avec l'id ${data.id} a été créé `)
+  }
 
   return (products &&
 
@@ -38,6 +57,7 @@ function App() {
                 <Card.Text>
                   <strong>Prix :</strong> {product.price} €
                 </Card.Text>
+                <Button variant="primary" onClick={handleAddProduct}>Ajouter un produit</Button>
               </Card.Body>
             </Card>
           </Col>
