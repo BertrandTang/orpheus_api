9e6e0701bf1fd507f891ba621fd2d42e470224b6
Bertrand <bertrand.tang.dev@gmail.com>
2025-06-24 10:56:29 +0200

    updates functions with error handling code

--- src/App.jsx
+++ src/App.jsx
@@ -8,81 +8,163 @@ import "../src/assets/styles/app.scss";
 
 function App() {
   const [products, setProducts] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     async function fetchProducts() {
-      const response = await fetch("https://fakestoreapi.com/products");
-      const data = await response.json();
-      setProducts(data);
+      try {
+        const response = await fetch("https://fakestoreapi.com/products");
+
+        if (!response.ok) {
+          throw new Error(
+            `Erreur HTTP: ${
+              response.statusText ? response.statusText + " - " : ""
+            }${response.status}`
+          );
+        }
+
+        const data = await response.json();
+        setProducts(data);
+      } catch (error) {
+        setError(
+          "Une erreur est survenue lors de la récupération des produits."
+        ); // Client
+        console.error(error.message); // Développeur
+      } finally {
+        // Désactive l'état de chargement, qu'il y ait une erreur ou non
+        setLoading(false);
+      }
     }
     fetchProducts();
   }, []);
 
-  async function handleAddProduct() {
-    const response = await fetch("https://fakestoreapi.com/products", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        title: "Naïnetendo Souitch 2",
-        price: 449.99,
-        description: "Un super produit ajouté via API",
-        image: "https://via.placeholder.com/150",
-        category: "electronics",
-      }),
-    });
-    const data = await response.json();
-    alert(` Le produit avec l'id ${data.id} a été créé `);
-  }
+  if (error) return <p>Erreur : {error}</p>;
+  if (loading) return <p>Chargement...</p>;
 
-  async function handleModifyProduct(productId) {
-    const response = await fetch(
-      `https://fakestoreapi.com/products/${productId}`,
-      {
-        method: "PUT",
+  async function handleAddProduct() {
+    try {
+      const response = await fetch("https://fakestoreapi.com/products", {
+        method: "POST",
         headers: {
           "Content-Type": "application/json",
         },
         body: JSON.stringify({
           title: "Naïnetendo Souitch 2",
-          price: 559.99, // Ligne à modifier
+          price: 449.99,
           description: "Un super produit ajouté via API",
           image: "https://via.placeholder.com/150",
           category: "electronics",
         }),
+      });
+
+      if (!response.ok) {
+        throw new Error(
+          `Erreur HTTP: ${
+            response.statusText ? response.statusText + " - " : ""
+          }${response.status}`
+        );
       }
-    );
-    const data = await response.json();
-    alert(` Le produit avec l'id ${data.id} a été modifié `);
+
+      const data = await response.json();
+      alert(`Le produit avec l'id ${data.id} a été créé`);
+    } catch (error) {
+      setError("Une erreur est survenue lors de l'ajout du produit.");
+      console.error(error.message);
+    }
+  }
+
+  async function handleModifyProduct(productId) {
+    try {
+      const response = await fetch(
+        `https://fakestoreapi.com/products/${productId}`,
+        {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            title: "Naïnetendo Souitch 2",
+            price: 559.99, // Ligne à modifier
+            description: "Un super produit ajouté via API",
+            image: "https://via.placeholder.com/150",
+            category: "electronics",
+          }),
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error(
+          `Erreur HTTP: ${
+            response.statusText ? response.statusText + " - " : ""
+          }${response.status}`
+        );
+      }
+
+      const data = await response.json();
+      alert(`Le produit avec l'id ${data.id} a été modifié`);
+    } catch (error) {
+      setError("Une erreur est survenue lors de la modification du produit.");
+      console.error(error.message);
+    }
   }
 
   async function handleModifyPriceProduct(productId) {
-    const response = await fetch(
-      `https://fakestoreapi.com/products/${productId}`,
-      {
-        method: "PATCH",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          price: 339.99, // Ligne à modifier
-        }),
+    try {
+      const response = await fetch(
+        `https://fakestoreapi.com/products/${productId}`,
+        {
+          method: "PATCH",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            price: 339.99, // Ligne à modifier
+          }),
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error(
+          `Erreur HTTP: ${
+            response.statusText ? response.statusText + " - " : ""
+          }${response.status}`
+        );
       }
-    );
-    const data = await response.json();
-    alert(` Le prix du produit avec l'id ${data.id} a été modifié `);
+
+      const data = await response.json();
+      alert(`Le prix du produit avec l'id ${data.id} a été modifié`);
+    } catch (error) {
+      setError(
+        "Une erreur est survenue lors de la modification du prix du produit."
+      );
+      console.error(error.message);
+    }
   }
 
   async function handleDeleteProduct(productId) {
-    const response = await fetch(
-      `https://fakestoreapi.com/products/${productId}`,
-      {
-        method: "DELETE",
+    try {
+      const response = await fetch(
+        `https://fakestoreapi.com/products/${productId}`,
+        {
+          method: "DELETE",
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error(
+          `Erreur HTTP: ${
+            response.statusText ? response.statusText + " - " : ""
+          }${response.status}`
+        );
       }
-    );
-    const data = await response.json();
-    alert(` Le produit avec l'id ${data.id} a été supprimé `);
+
+      const data = await response.json();
+      alert(`Le produit avec l'id ${data.id} a été supprimé`);
+    } catch (error) {
+      setError("Une erreur est survenue lors de la suppression du produit.");
+      console.error(error.message);
+    }
   }
 
   return (
